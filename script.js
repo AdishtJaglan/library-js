35f5362f855490cfdef9674701bfd43b16602039
AdishtJaglan <adisht7524@gmail.com>
2024-03-23 22:41:43 +0530

    implement logic to create and push new book to array

--- script.js
+++ script.js
@@ -1,1 +1,37 @@
-console.log("connected!");
+const myLibrary = [
+    {
+        title: "Can't Hurt Me",
+        author: "David Goggins",
+        pages: 312,
+        read: true,
+    },
+    {
+        title: "Atomic Habits",
+        author: "James Clear",
+        pages: 243,
+        read: true,
+    },
+    {
+        title: "Origin",
+        author: "Dan Brown",
+        pages: 458,
+        read: false,
+    },
+];
+
+function Book(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+}
+
+function addBookToLibrary(title, author, pages, read) {
+    const newBook = new Book(title, author, pages, read)
+
+    myLibrary.push(newBook);
+
+    return myLibrary;
+}
+
+console.log(addBookToLibrary("some title", "some author", 234, false));

8c027241d922d2379417323962cdb0d1325679de
AdishtJaglan <adisht7524@gmail.com>
2024-03-23 23:08:42 +0530

    displaying book info in html

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+const list = document.querySelector("ul");
 const myLibrary = [
     {
         title: "Can't Hurt Me",
@@ -19,6 +20,20 @@ const myLibrary = [
     },
 ];
 
+for (let i = 0; i < myLibrary.length; i++) {
+    const li = document.createElement("li");
+
+    li.innerHTML = `
+        <div class="card">
+            <p class="title">Title: ${myLibrary[i].title} </p>
+            <p class="author">Author: ${myLibrary[i].author}</p>
+            <p class="page">Page: ${myLibrary[i].pages}</p>
+            <p class="read">Book Read: ${myLibrary[i].read ? "yes" : "no"}</p>
+        </div>`
+
+    list.appendChild(li);
+}
+
 function Book(title, author, pages, read) {
     this.title = title;
     this.author = author;
@@ -34,4 +49,4 @@ function addBookToLibrary(title, author, pages, read) {
     return myLibrary;
 }
 
-console.log(addBookToLibrary("some title", "some author", 234, false));
+// console.log(addBookToLibrary("some title", "some author", 234, false));
